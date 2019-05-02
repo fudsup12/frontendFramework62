5cb20c89eff1e644a68f9108952e35dc034c1012
patnaree <patnaree1146@mail.com>
2019-05-02 14:09:02 +0700

    deployedit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files form the dist directorydfdf
 app.use(express.static("./dist/frontendFramework62"));
 
 app.get("/*", function(req, res) {
